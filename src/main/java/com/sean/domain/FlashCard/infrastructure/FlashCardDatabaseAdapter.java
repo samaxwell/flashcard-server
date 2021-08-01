82851752d051e7add35f7471af0110a1608f45db
Sean Maxwell <sa.maxwell@yahoo.com>
2021-07-31 19:53:13 -0500

    Added error handling

--- src/main/java/com/sean/domain/FlashCard/infrastructure/FlashCardDatabaseAdapter.java
+++ src/main/java/com/sean/domain/FlashCard/infrastructure/FlashCardDatabaseAdapter.java
@@ -10,7 +10,6 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
